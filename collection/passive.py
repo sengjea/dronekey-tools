9e2e8fc4c72015f0b6ae3e2c1b2b0d7c0848bf46
Gregory Lyras <greglyras@gmail.com>
2014-07-01 16:12:30 +0100

    minor corrections on passive collection script

--- collection/passive.py
+++ collection/passive.py
@@ -4,7 +4,7 @@
 #* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 # File Name : passive.py
 # Creation Date : 26-06-2014
-# Last Modified : Tue 01 Jul 2014 03:53:10 PM BST
+# Last Modified : Tue 01 Jul 2014 04:12:12 PM BST
 # Created By : Greg Lyras <greglyras@gmail.com>
 #_._._._._._._._._._._._._._._._._._._._._.*/
 
@@ -42,36 +42,27 @@ def rssi(lines):
       return float(rssi)
 
 
-def get_full_line():
-
-
-def main():
+def get_file_data():
   inpt_dev = open(INFO_FILE)
   inpt_wifi = open(WIRELESS_FILE)
-  ts_previous = timestamp()
+  ts = timestamp()
   inpt_dev_lines = inpt_dev.readlines()
   inpt_wifi_lines = inpt_wifi.readlines()
   inpt_dev.close()
   inpt_wifi.close()
+  return (ts, inpt_dev_lines, inpt_wifi_lines)
+
+
+
+def main():
+  (ts_previous, inpt_dev_lines, inpt_wifi_lines) = get_file_data()
   previous = pkts_received(inpt_dev_lines)
-  print "{0},\t{1},\t{2}".format("Timestamp", "RSSI", "PRR")
+  print "{0},\t{1},\t{2}".format("Timestamp", "RSSI", "Received")
   while True:
-    sleep(1)
-    inpt_dev = open(INFO_FILE)
-    inpt_wifi = open(WIRELESS_FILE)
-    ts = timestamp()
-    inpt_dev_lines = inpt_dev.readlines()
-    inpt_wifi_lines = inpt_wifi.readlines()
-    inpt_dev.close()
-    inpt_wifi.close()
-
-    received = pkts_received(inpt_dev_lines)
-
-    prr = (received - previous) / (ts - ts_previous)
-    previous = received
-    ts_previous = ts
+    (ts, inpt_dev_lines, inpt_wifi_lines) = get_file_data()
+    received = pkts_received(inpt_dev_lines) - previous
     rssi_value = rssi(inpt_wifi_lines)
-    print "{0},\t{1},\t{2}".format(ts, rssi_value, prr)
+    print "{0},\t{1},\t{2}".format(ts, rssi_value, received)
 
 
 if __name__=="__main__":
