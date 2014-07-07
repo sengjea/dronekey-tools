c938d8c6d9085bdc5e39ae99285c9d154d42728d
Gregory Lyras <greglyras@gmail.com>
2014-07-07 15:44:40 +0100

    added the rssi scripts in a seperate folder

---
+++ collection/rssi/iperf/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+#
+#* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+# File Name : __init__.py
+# Creation Date : 01-07-2014
+# Last Modified : Tue 01 Jul 2014 07:28:08 PM BST
+# Created By : Greg Lyras <greglyras@gmail.com>
+#_._._._._._._._._._._._._._._._._._._._._.*/
+
+import iperf
