bd54168611b9aa5104f00568d088ce5fe8f461bc
Gregory Lyras <greglyras@gmail.com>
2014-07-08 19:12:16 +0100

    minor compatibility fix in str format method

--- movement/UAV/waypoints/uav_collection_points.py
+++ movement/UAV/waypoints/uav_collection_points.py
@@ -7,20 +7,20 @@ class coords:
 		self.z = z
 
 	def __str__(self):
-		return "({},{},{})".format(self.x, self.y, self.z)
+		return "({0}, {1}, {2})".format(self.x, self.y, self.z)
 
 def get_points(origin, radius, theta_step, phi_step):
-	point_list = []	
+	point_list = []
 	for iter_p in range(5,90,phi_step):
 		phi = math.radians(90 - iter_p)
 		for iter_t in range(5,360,theta_step):
-			theta = math.radians(iter_t)	
-			dest = copy.deepcopy(origin)	
+			theta = math.radians(iter_t)
+			dest = copy.deepcopy(origin)
 			dest.x += radius * math.sin(phi) * math.cos(theta)
 			dest.y += radius * math.sin(phi) * math.sin(theta)
 			dest.z += radius * math.cos(phi)
 			point_list.append(dest)
-	return point_list 
+	return point_list
 
 if __name__ == "__main__":
 	ap_location = coords(0,0,0)
